73b4fc9e22ea60e002ac749bdd749407900f0ef0
Ben Drucker <bvdrucker@gmail.com>
2020-05-04 22:56:03 -0700

    add FormatURLFunc

--- lang/funcs/url.go
+++ lang/funcs/url.go
@@ -31,17 +31,23 @@ var ParseURLFunc = function.New(&function.Spec{
 			return cty.NilVal, err
 		}
 
-		var password cty.Value
-		v, ok := url.User.Password()
-		if ok {
-			password = cty.StringVal(v)
-		} else {
+		var username, password cty.Value
+		if url.User == nil {
+			username = cty.NullVal(cty.String)
 			password = cty.NullVal(cty.String)
+		} else {
+			username = cty.StringVal(url.User.Username())
+			v, ok := url.User.Password()
+			if ok {
+				password = cty.StringVal(v)
+			} else {
+				password = cty.NullVal(cty.String)
+			}
 		}
 
 		return cty.ObjectVal(map[string]cty.Value{
 			"scheme":   cty.StringVal(url.Scheme),
-			"username": cty.StringVal(url.User.Username()),
+			"username": username,
 			"password": password,
 			"host":     cty.StringVal(url.Host),
 			"path":     cty.StringVal(url.Path),
@@ -50,7 +56,52 @@ var ParseURLFunc = function.New(&function.Spec{
 	},
 })
 
+// FormatURLFunc constructs a function that formats a URL object to a string.
+var FormatURLFunc = function.New(&function.Spec{
+	Params: []function.Parameter{
+		{
+			Name: "url",
+			Type: cty.Object(map[string]cty.Type{
+				"scheme":   cty.String,
+				"username": cty.String,
+				"password": cty.String,
+				"host":     cty.String,
+				"path":     cty.String,
+				"query":    cty.String,
+			}),
+		},
+	},
+	Type: function.StaticReturnType(cty.String),
+	Impl: func(args []cty.Value, retType cty.Type) (ret cty.Value, err error) {
+		obj := args[0].AsValueMap()
+
+		var user *url.Userinfo
+		if !obj["user"].IsNull() {
+			if obj["password"].IsNull() {
+				user = url.User(obj["username"].AsString())
+			} else {
+				user = url.UserPassword(obj["username"].AsString(), obj["password"].AsString())
+			}
+		}
+
+		u := &url.URL{
+			Scheme:   obj["scheme"].AsString(),
+			User:     user,
+			Host:     obj["host"].AsString(),
+			Path:     obj["path"].AsString(),
+			RawQuery: obj["query"].AsString(),
+		}
+
+		return cty.StringVal(u.String()), nil
+	},
+})
+
 // ParseURL parses a URL, returning a URL object.
 func ParseURL(url cty.Value) (cty.Value, error) {
 	return ParseURLFunc.Call([]cty.Value{url})
+}
+
+// FormatURL formats a URL object, returning a string.
+func FormatURL(url cty.Value) (cty.Value, error) {
+	return FormatURLFunc.Call([]cty.Value{url})
 }
--- lang/funcs/url_test.go
+++ lang/funcs/url_test.go
@@ -17,7 +17,7 @@ func TestParseURL(t *testing.T) {
 			cty.StringVal("https://app.terraform.io/app?q=1"),
 			cty.ObjectVal(map[string]cty.Value{
 				"scheme":   cty.StringVal("https"),
-				"username": cty.StringVal(""),
+				"username": cty.NullVal(cty.String),
 				"password": cty.NullVal(cty.String),
 				"host":     cty.StringVal("app.terraform.io"),
 				"path":     cty.StringVal("/app"),
@@ -30,6 +30,46 @@ func TestParseURL(t *testing.T) {
 	for _, test := range tests {
 		t.Run(fmt.Sprintf("parseurl(%#v)", test.URL), func(t *testing.T) {
 			got, err := ParseURL(test.URL)
+
+			if test.Err {
+				if err == nil {
+					t.Fatal("succeeded; want error")
+				}
+				return
+			} else if err != nil {
+				t.Fatalf("unexpected error: %s", err)
+			}
+
+			if !got.RawEquals(test.Want) {
+				t.Errorf("wrong result\ngot:  %#v\nwant: %#v", got, test.Want)
+			}
+		})
+	}
+}
+
+func TestFormatURL(t *testing.T) {
+	tests := []struct {
+		URL  cty.Value
+		Want cty.Value
+		Err  bool
+	}{
+		{
+			cty.ObjectVal(map[string]cty.Value{
+				"scheme":   cty.StringVal("https"),
+				"username": cty.NullVal(cty.String),
+				"password": cty.NullVal(cty.String),
+				"host":     cty.StringVal("app.terraform.io"),
+				"path":     cty.StringVal("/app"),
+				"query":    cty.StringVal("q=1"),
+			}),
+			cty.StringVal("https://app.terraform.io/app?q=1"),
+			false,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(fmt.Sprintf("parseurl(%#v)", test.URL), func(t *testing.T) {
+			got, err := FormatURL(test.URL)
 
 			if test.Err {
 				if err == nil {
